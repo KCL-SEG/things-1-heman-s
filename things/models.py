e1de0e89e7726f3e5eb9b0533bbe328ccd30fc06
Heman Seegolam <heman.seegolam@kcl.ac.uk>
2022-12-03 21:03:26 +0000

    Complete Exercise 2

---
+++ things/models.py
@@ -0,0 +1,7 @@
+from django.db import models
+from django.db.models import Model
+
+class Thing(Model):
+    name = models.TextField()
+    description = models.TextField()
+    quanitity = models.TextField()
